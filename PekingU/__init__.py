2713d842cb0e5784194bbd84afb4cf78324f0e82
12号娱乐选手 <127727842+MinghengZhong@users.noreply.github.com>
2024-03-06 21:10:54 +0800

    Add files via upload

---
+++ PekingU/__init__.py
@@ -0,0 +1,12 @@
+def who(s):
+    if s == 'cyx' or s == 'cqj' or s == 'xbh':
+        return '%sgg tql!!!' % s
+    elif s == 'zmh':
+        return 'SB'
+    else:
+        return 'I don\'t know %s.' % s
+
+
+def cqjgg():
+    while True:
+        print('cqjgg tql!!!')
